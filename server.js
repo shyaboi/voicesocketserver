2bfcc003ff0b358bf870ec667ae089179d931aa6
Shyaboi <ian.shane.sears@gmail.com>
2020-10-28 15:04:10 -0700

    initial commit, sockets just passivly passing data buffers

---
+++ server.js
@@ -0,0 +1,27 @@
+const PORT = process.env.PORT || 8888
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(1));
+//   res.end(content);
+});
+const io = require('socket.io')(httpServer);
+io.on('connect', socket => {
+
+
+
+    socket.on('video', data => {
+        console.log(data)
+      socket.broadcast.emit('got', data)
+    });
+
+
+
+
+
+
+    
+  });
+httpServer.listen(PORT, () => {
+  console.log('go to http://localhost:'+PORT);
+});
