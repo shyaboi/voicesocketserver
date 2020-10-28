dc1ef920a99c7bb8145778bfa8ed9fcbe980b703
Shyaboi <ian.shane.sears@gmail.com>
2020-10-28 15:19:09 -0700

    added html to server to endpoint

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const PORT = process.env.PORT || 8888
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
 const httpServer = require('http').createServer((req, res) => {
   // serve the index.html file
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Content-Length', Buffer.byteLength(1));
-  res.end('hiwrlkd');
+  res.end(content);
 });
 const io = require('socket.io')(httpServer);
 io.on('connect', socket => {
